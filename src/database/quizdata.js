ca2aa4a85238833f1a605a6ffc2291b3cc0bb595
Veeresh B V <Veereshbv04@gmail.com>
2022-06-13 10:13:40 +0530

    faet: database added

---
+++ src/database/quizdata.js
@@ -0,0 +1,67 @@
+const questions = [
+    {   
+        id:1,
+        category:"js quiz",
+        questions:[
+            {
+                questionText:"who developed JavaScipt",
+                optionsText:[
+                    {option:"Bill gates", isCorrect:false},
+                    {option:"henry ford", isCorrect:false},
+                    {option:"William james", isCorrect:true},
+                    {option:"James WIlliam", isCorrect:false}
+                ]
+            },
+            {
+                questionText:"who developed Tyesscript",
+                optionsText:[
+                    {option:"typesl gates", isCorrect:false},
+                    {option:"helliamry ford", isCorrect:false},
+                    {option:"james", isCorrect:true},
+                    {option:"James WIlliam", isCorrect:false}
+                ]
+            },
+            {
+                questionText:"who developed vscode",
+                optionsText:[
+                    {option:"Microsoft", isCorrect:false},
+                    {option:"oracle", isCorrect:false},
+                    {option:"William james", isCorrect:true},
+                    {option:"James WIlliam", isCorrect:false}
+                ]
+            }
+        ]
+    },
+    {
+        id: 2,
+        category: "general",
+        questions: [{
+                questionText: "who prime minister of India",
+                optionsText: [
+                    {option: "Naredra Modi",isCorrect: true},
+                    {option: "henry ford",   isCorrect: false},
+                    {option: "William james",isCorrect: false},
+                    {option: "James WIlliam",isCorrect: false}
+                ]
+            },
+            {
+                questionText: "who is education minister of india",
+                optionsText: [
+                    {option: "typesl gates",isCorrect: false},
+                    {option: "helliamry ford",isCorrect: false},
+                    {option: "james", isCorrect: true},
+                    {option: "James WIlliam",isCorrect: false}
+                ]
+            },
+            {
+                questionText: "who developed vscode",
+                optionsText: [
+                    {option: "Microsoft",   isCorrect: false},
+                    {option: "oracle",   isCorrect: false},
+                    {option: "William james",   isCorrect: true},
+                    {option: "James WIlliam",   isCorrect: false}
+                ]
+            }
+        ]
+    }
+]
