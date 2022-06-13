a7e07c8115f17d3df58ff10cd9c4361f72269357
Veeresh B V <Veereshbv04@gmail.com>
2022-06-13 11:55:28 +0530

    feat: router

---
+++ src/router/router.jsx
@@ -0,0 +1,12 @@
+import {Routes, Route,} from "react-router-dom";
+import HomePage from "../pages/HomePage";
+
+export default function Router(){
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            
+        </Routes>
+    )
+    
+}
